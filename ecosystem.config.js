97728327981abd1ce52ce8effe4546adb6e83d9f
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-02-09 13:53:20 +0100

    chore: fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
 	apps: [
 		{
-			name: 'OCR-Bot',
+			name: 'PinBot',
 			script: 'bot.js',
 
 			// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
 			args: 'one two',
 			instances: 1,
 			autorestart: true,
-			watch: ['bot.js', 'commands', 'modules'],
+			watch: ['bot.js', 'commands'],
 			max_memory_restart: '1G',
 			env: {
 				NODE_ENV: 'development',
